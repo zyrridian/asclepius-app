5147d09c4107a3b9caaafb570dc30858ab6e3cdb
zyrridian <rezkyaf246@gmail.com>
2024-10-10 15:10:41 +0700

    feat: add image classifier from gallery

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,19 +35,28 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        mlModelBinding = true
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
 
     // TODO: Tambahkan Library TensorFlow Lite
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
+
+    // Camera
+//    implementation("androidx.camera:camera-camera2:1.3.4")
+//    implementation("androidx.camera:camera-lifecycle:1.3.4")
+//    implementation("androidx.camera:camera-view:1.3.4")
 }
--- app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
@@ -2,7 +2,6 @@ package com.dicoding.asclepius.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
@@ -10,10 +9,17 @@ class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // TODO: Menampilkan hasil gambar, prediksi, dan confidence score.
+        binding.resultText.text = intent.getStringExtra(EXTRA_RESULT)
+        binding.resultImage.setImageURI(intent.getParcelableExtra(EXTRA_IMAGE_URI))
     }
 
+    companion object {
+        const val EXTRA_IMAGE_URI = "uri"
+        const val EXTRA_RESULT = "result"
+    }
 
 }
